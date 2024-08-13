5aeffb5538d7bfc63c04ff8a78b91f69e5efb567
Nipuna Sudesh <142614223+NipunaSudesh@users.noreply.github.com>
2024-08-13 15:27:31 +0530

    created  Game Character

--- src/GameCharacter.java
+++ src/GameCharacter.java
@@ -1,4 +1,25 @@
 public class GameCharacter {
     private String name;
     private int health;
+
+    public String getName(){
+        return name;
+    }
+    public int getHealth(){
+        return health;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+    public void setHealth(int Health){
+        this.health=health;
+    }
+    public void loosHealth( int discressHealth){
+        this.setHealth(this.getHealth()-discressHealth);
+        if(this.getHealth()<=0){
+            System.out.println(this.getName() +"died!");
+        }else if(this.getHealth()>0){
+            System.out.println(this.getName() +"health discrese to "+this.getHealth());
+        }
+    }
 }
